8f6ac1332817f94d54f40bf9ded61f5800561b91
dmitriyakkerman <dmitriyakkerman@gmail.com>
2021-07-29 21:40:52 +0300

    UMD + README upd

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 # anime-in-viewport
 
-**Simple example:**
+**Usage**
+
+```js 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js" integrity="sha512-z4OUqw38qNLpn1libAN9BsoDx6nbNFio5lA6CuTp9NlK83b89hgyCVq+N5FdBJptINztxn1Z3SaKSKUS5UP60Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="./dist/anime-in-viewport.min.js"></script>
+```
+
+**Example:**
 ```js 
  AnimeInViewport.init('.item', (el, i) => ({
-     translateX: 250,
-     delay: () => i * 100
+     translateY: ['100px', 0],
+     opacity: [0, 1],
+     easing: "easeOutExpo",
+     duration: 1000,
+     delay: () => {
+         return i * 300
+     }
  }), '0px', 1.0)
 ```
      
--- src/anime-in-viewport.js
+++ src/anime-in-viewport.js
@@ -1,60 +1,73 @@
 import { defaults } from "./defaults";
 
-class AnimeInViewport {
-    constructor(targets, options, rootMargin, threshold) {
-
-        if(typeof targets !== 'string') {
-            throw new Error('Type of "targets" option should be a "string"');
-        }
-
-        if (typeof options !== 'function') {
-            throw new Error('Specify animation options');
-        }
-
-        this.targets = [targets];
-        this.options = options;
-        this.rootMargin = rootMargin;
-        this.threshold = threshold;
-        this.init();
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory();
+    } else {
+        root.AnimeInViewport = factory();
     }
+}(typeof self !== 'undefined' ? self : this, function () {
 
-    init() {
-        let that = this;
+    class AnimeInViewport {
+        constructor(targets, options, rootMargin, threshold) {
 
-        that.targets.forEach(function(targetElement) {
-            let observer = new IntersectionObserver(
-                function(entries, observer) {
-                    entries.forEach((entry, index) => {
-                        if(entry.intersectionRatio > 0) {
-                            if(!entry.target.classList.contains('animated')) {
-                                that.mergeOptions(entry, index, entries.length);
-                                entry.target.classList.add('animated');
+            if(typeof targets !== 'string') {
+                throw new Error('Type of "targets" option should be a "string"');
+            }
+
+            if (typeof options !== 'function') {
+                throw new Error('Specify animation options');
+            }
+
+            this.targets = [targets];
+            this.options = options;
+            this.rootMargin = rootMargin;
+            this.threshold = threshold;
+            this.init();
+        }
+
+        init() {
+            let that = this;
+
+            that.targets.forEach(function(targetElement) {
+                let observer = new IntersectionObserver(
+                    function(entries, observer) {
+                        entries.forEach((entry, index) => {
+                            if(entry.intersectionRatio > 0) {
+                                if(!entry.target.classList.contains('animated')) {
+                                    that.mergeOptions(entry, index, entries.length);
+                                    entry.target.classList.add('animated');
+                                }
                             }
-                        }
+                        });
+                    },
+                    {
+                        rootMargin: that.rootMargin || defaults.rootMargin,
+                        threshold: that.threshold || defaults.threshold,
                     });
-                },
-                {
-                    rootMargin: that.rootMargin || defaults.rootMargin,
-                    threshold: that.threshold || defaults.threshold,
+
+                let targets = document.querySelectorAll(targetElement);
+                targets.forEach(function(target) {
+                    observer.observe(target);
                 });
 
-            let targets = document.querySelectorAll(targetElement);
-            targets.forEach(function(target) {
-                observer.observe(target);
-            });
+            })
+        }
 
-        })
+        mergeOptions(entry, index, length) {
+            anime(
+                Object.assign({ targets: entry.target }, this.options(entry, index, length))
+            );
+        }
     }
 
-    mergeOptions(entry, index, length) {
-        anime(
-            Object.assign({ targets: entry.target }, this.options(entry, index, length))
-        );
-    }
-}
+    AnimeInViewport.init = function(targets, options, rootMargin, threshold) {
+        return new AnimeInViewport(targets, options, rootMargin, threshold)
+    };
 
-AnimeInViewport.init = function(targets, options, rootMargin, threshold) {
-    return new AnimeInViewport(targets, options, rootMargin, threshold)
-};
+    window.AnimeInViewport = AnimeInViewport;
 
-window.AnimeInViewport = AnimeInViewport;
+    return AnimeInViewport;
+}));
